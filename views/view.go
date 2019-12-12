7c5feca9b1c889d01cda0aa7787af2bad2f166a4
stan wielga <svwielga4@gmail.com>
2019-12-12 13:32:44 -0800

    refactored views package

--- views/view.go
+++ views/view.go
@@ -1,16 +1,19 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	LayoutDir   string = "views/layout/"
+	TemplateExt string = ".gohtml"
+)
 
 // NewView accepts a list of strings and returns some views, it should
 // only be used during setup and not runtime
 func NewView(layout string, files ...string) *View {
-	files = append(files,
-		"views/layout/bootstrap.gohtml",
-		"views/layout/navbar.gohtml",
-		"views/layout/footer.gohtml",
-	)
-
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
@@ -25,4 +28,13 @@ func NewView(layout string, files ...string) *View {
 type View struct {
 	Template *template.Template
 	Layout   string
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
+	if err != nil {
+		panic(err)
+	}
+
+	return files
 }
