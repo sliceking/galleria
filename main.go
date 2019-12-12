b6e3245f9e169d5ab956ea0963dec61a241ce64d
stan wielga <svwielga4@gmail.com>
2019-12-12 13:13:38 -0800

    added navbar

--- main.go
+++ main.go
@@ -1,45 +1,34 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sliceking/galleria/views"
 )
 
-var homeTemplate *template.Template
-var contactTemplate *template.Template
+var homeView *views.View
+var contactView *views.View
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeTemplate.Execute(w, nil); err != nil {
+	err := homeView.Template.ExecuteTemplate(w, homeView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := contactTemplate.Execute(w, nil); err != nil {
+	err := contactView.Template.ExecuteTemplate(w, contactView.Layout, nil)
+	if err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	var err error
-	homeTemplate, err = template.ParseFiles(
-		"views/home.gohtml",
-		"views/layout/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
-	contactTemplate, err = template.ParseFiles(
-		"views/contact.gohtml",
-		"views/layout/footer.gohtml",
-	)
-	if err != nil {
-		panic(err)
-	}
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
--- views/view.go
+++ views/view.go
@@ -4,8 +4,12 @@ import "html/template"
 
 // NewView accepts a list of strings and returns some views, it should
 // only be used during setup and not runtime
-func NewView(files ...string) *View {
-	files = append(files, "views/layout/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files,
+		"views/layout/bootstrap.gohtml",
+		"views/layout/navbar.gohtml",
+		"views/layout/footer.gohtml",
+	)
 
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -14,9 +18,11 @@ func NewView(files ...string) *View {
 
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
