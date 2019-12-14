26c4ba64bc13a99a280ec5920c6ae5608eba23ad
stan wielga <svwielga4@gmail.com>
2019-12-14 15:10:39 -0800

    drying up controller helper

--- controllers/users.go
+++ controllers/users.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"github.com/sliceking/galleria/views"
 )
 
@@ -37,16 +36,9 @@ type SignupForm struct {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		fmt.Fprintf(w, "Bad Request")
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-
 	fmt.Fprintln(w, form)
 }
