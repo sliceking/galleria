7f2d304dbf5f4d0bf55836262b31387703f0732b
stan wielga <svwielga4@gmail.com>
2019-12-15 15:23:25 -0800

    experimenting wth gorm

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"database/sql"
+	"bufio"
 	"fmt"
+	"os"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -14,29 +16,48 @@ const (
 	dbname = "galleria_dev"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+	Color string
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname,
 	)
 
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	var id int
-	err = db.QueryRow(`
-		INSERT INTO users(name, email)
-		VALUES($1, $2)
-		RETURNING id`,
-		"sjon scalhoun",
-		"sjon@scalhoun.io",
-	).Scan(&id)
-	if err != nil {
+	if err = db.DB().Ping(); err != nil {
 		panic(err)
 	}
+	db.LogMode(true)
+	db.AutoMigrate(&User{})
+	// db.DropTableIfExists(&User{})
 
-	fmt.Println("ID iis:", id)
+	name, email, color := getInfo()
+	u := &User{
+		Name:  name,
+		Email: email,
+		Color: color,
+	}
+	db.Create(&u)
+	fmt.Println(u)
+}
 
+func getInfo() (name, email, color string) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("What is your name?")
+	name, _ = reader.ReadString('\n')
+	fmt.Println("What is your email?")
+	email, _ = reader.ReadString('\n')
+	fmt.Println("What is your color?")
+	color, _ = reader.ReadString('\n')
+	return name, email, color
 }
