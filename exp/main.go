aa65591972bbfa0654d580b53a10e798e8e5c270
svwielga4 <svwielga4@gmail.com>
2019-12-16 13:38:39 -0800

    added userservice for interacting with user models

--- exp/main.go
+++ exp/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/sliceking/galleria/models"
 )
 
 const (
@@ -14,56 +14,21 @@ const (
 	dbname = "galleria_dev"
 )
 
-type User struct {
-	gorm.Model
-	Name  string
-	Email string `gorm:"not null;unique_index"`
-	Color string
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
 		host, port, user, dbname,
 	)
 
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
-	if err = db.DB().Ping(); err != nil {
-		panic(err)
-	}
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-	// db.DropTableIfExists(&User{})
-
-	var u User
-	if err := db.First(&u).Error; err != nil {
-		panic(err)
-	}
-
-	createOrder(db, u, 1001, "Fake Desc1")
-	createOrder(db, u, 54545, "Fake Desc2")
-	createOrder(db, u, 235543453, "Fake Desc3")
-	createOrder(db, u, 8877, "Fake Desc4")
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	err := db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	}).Error
+	defer us.Close()
+	user, err := us.ByID(2)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(user)
+
 }
