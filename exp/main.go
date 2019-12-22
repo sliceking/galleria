95ea214c2a81802c114913ad0e6bd219938cbcb2
stan wielga <svwielga4@gmail.com>
2019-12-22 12:36:32 -0800

    added the rand package that allows to create random strings and random remember tokens

--- exp/main.go
+++ exp/main.go
@@ -3,50 +3,10 @@ package main
 import (
 	"fmt"
 
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/sliceking/galleria/models"
-)
-
-const (
-	host   = "localhost"
-	port   = 5432
-	user   = "stanwielga"
-	dbname = "galleria_dev"
+	"github.com/sliceking/galleria/rand"
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		host, port, user, dbname,
-	)
-
-	us, err := models.NewUserService(psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer us.Close()
-	us.AutoMigrate()
-
-	u := models.User{
-		Name:  "superasdfs man",
-		Email: "super@man.com",
-	}
-
-	if err := us.Create(&u); err != nil {
-		panic(err)
-	}
-
-	// u.Email = "paper@person.com"
-	// if err = us.Update(&u); err != nil {
-	// 	panic(err)
-	// }
-
-	if err := us.Delete(u.ID); err != nil {
-		panic(err)
-	}
-
-	userByID, err := us.ByID(u.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(userByID)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
---
+++ rand/strings.go
@@ -0,0 +1,36 @@
+package rand
+
+import "crypto/rand"
+
+import "encoding/base64"
+
+const RememberTokenBytes = 32
+
+// Bytes will help us generate n random bytes, or will return an error
+// if there was one
+func Bytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// String will generate a byte slice of nbytes and return a string that is the
+// base64 urlencoded version of that byte slice
+func String(nBytes int) (string, error) {
+	b, err := Bytes(nBytes)
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
+// RememberToken is a helper function designed to generate remember tokens
+// of a predetermined byte size.
+func RememberToken() (string, error) {
+	return String(RememberTokenBytes)
+}
