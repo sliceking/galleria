68e99a233cf1282f082f9ef9301004f3cda600d8
stan wielga <svwielga4@gmail.com>
2020-03-06 18:56:39 -0500

    adding the hash package

--- exp/main.go
+++ exp/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 
-	"github.com/sliceking/galleria/rand"
+	"github.com/sliceking/galleria/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	toHash := []byte("this string to hash")
+	h := hmac.New(sha256.New, []byte("my-secret-key"))
+	h.Write(toHash)
+	b := h.Sum(nil)
+	fmt.Println(base64.URLEncoding.EncodeToString(b))
+
+	hmac := hash.NewHMAC("my-secret-key")
+	fmt.Println(hmac.Hash("this string to hash"))
+
 }
