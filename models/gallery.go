2e2f7b66019873e74c5fb5b0280eaca91cd10402
stan wielga <svwielga4@gmail.com>
2020-03-21 16:42:32 -0400

    adds services type

--- models/gallery.go
+++ models/gallery.go
@@ -1,10 +1,20 @@
 package models
 
-import 	"github.com/jinzhu/gorm"
+import "github.com/jinzhu/gorm"
 
 // Gallery is an our image container resource
 type Gallery struct {
 	gorm.Model
-	UserID uint `gorm:"not_null;index"`
-	Title string `gorm:"not_null"`
+	UserID uint   `gorm:"not_null;index"`
+	Title  string `gorm:"not_null"`
+}
+
+type GalleryService interface{}
+
+type GalleryDB interface {
+	Create(gallery *Gallery) error
+}
+
+type galleryGorm struct {
+	db *gorm.DB
 }
