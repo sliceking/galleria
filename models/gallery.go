d6f7b8fb548f747aa3f7b3ca9a12f8a68dd52664
stan wielga <svwielga4@gmail.com>
2020-03-21 19:40:03 -0400

    updates the gallery service

--- models/gallery.go
+++ models/gallery.go
@@ -9,12 +9,34 @@ type Gallery struct {
 	Title  string `gorm:"not_null"`
 }
 
-type GalleryService interface{}
+type GalleryService interface {
+	GalleryDB
+}
 
 type GalleryDB interface {
 	Create(gallery *Gallery) error
 }
 
+func NewGalleryService(db *gorm.DB) GalleryService {
+	return &galleryService{
+		GalleryDB: &galleryValidator{&galleryGorm{db}},
+	}
+}
+
+type galleryValidator struct {
+	GalleryDB
+}
+
+type galleryService struct {
+	GalleryDB
+}
+
+var _ GalleryDB = &galleryGorm{}
+
 type galleryGorm struct {
 	db *gorm.DB
+}
+
+func (gg *galleryGorm) Create(gallery *Gallery) error {
+	return gg.db.Create(gallery).Error
 }
