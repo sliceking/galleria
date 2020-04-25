b8cf9cfab67d1da90dbe6c5a23f71466ca853bdf
Stan Wielga <svwielga4@gmail.com>
2020-04-25 16:43:15 -0400

    added json config

--- config.go
+++ config.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type PostgresConfig struct {
 	Host     string `json:"host"`
@@ -37,21 +41,47 @@ func (c PostgresConfig) Dialect() string {
 }
 
 type Config struct {
-	Port    int    `json:"port"`
-	Env     string `json:"env"`
-	Pepper  string `json:"pepper"`
-	HMACkey string `json:"hmac_key"`
+	Port     int            `json:"port"`
+	Env      string         `json:"env"`
+	Pepper   string         `json:"pepper"`
+	HMACkey  string         `json:"hmac_key"`
+	Database PostgresConfig `json:"database"`
 }
 
 func DefaultConfig() Config {
 	return Config{
-		Port:    3000,
-		Env:     "dev",
-		Pepper:  "IamAsuperSecretString",
-		HMACkey: "secret-hmac-key",
+		Port:     3000,
+		Env:      "dev",
+		Pepper:   "IamAsuperSecretString",
+		HMACkey:  "secret-hmac-key",
+		Database: DefaultPostgresConfig(),
 	}
 }
 
 func (c Config) IsProd() bool {
 	return c.Env == "prod"
+}
+
+func LoadConfig(configRequired bool) Config {
+	f, err := os.Open(".config")
+	if err != nil {
+		if configRequired {
+			panic(err)
+		}
+
+		fmt.Println("Using default config...")
+
+		return DefaultConfig()
+	}
+
+	var c Config
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&c)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully loaded config")
+
+	return c
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
@@ -13,8 +14,11 @@ import (
 )
 
 func main() {
-	cfg := DefaultConfig()
-	dbCfg := DefaultPostgresConfig()
+	prodFlag := flag.Bool("prod", false, "Use this flag in production, to ensure a .config file is provided before the application starts")
+	flag.Parse()
+
+	cfg := LoadConfig(*prodFlag)
+	dbCfg := cfg.Database
 	services, err := models.NewServices(
 		models.WithGorm(dbCfg.Dialect(), dbCfg.ConnectionInfo()),
 		models.WithLogMode(!cfg.IsProd()),
